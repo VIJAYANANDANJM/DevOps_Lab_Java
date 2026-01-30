38b259d38ff14f0c201c7fad4f15954634ae9fd3
VIJAYANANDANJM <vijayedits2005@gmail.com>
2026-01-30 09:39:13 +0530

    Modified the code such that the hello world gets printed 10 times

--- hw.java
+++ hw.java
@@ -3,7 +3,8 @@ import java.util.*;
 
 class hw{
 	public static void main(String args[]){
-		System.out.println("Hello World!!! ");
+		for(int i=0;i<10;i++)
+		System.out.println("Hello World!!! "+(i+1));
 	}
 }
 
